7f067b84515360d1c145376afff9a8736425ac63
Gayan Chathuranga <gayan@datacaptureexperts.com.au>
2022-12-10 11:08:54 +0530

    load template

---
+++ WEB/src/app/views/pages/playHall/playHall.component.spec.ts
@@ -0,0 +1,35 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ButtonModule, CardModule, FormModule, GridModule } from '@coreui/angular';
+import { IconModule } from '@coreui/icons-angular';
+import { IconSetService } from '@coreui/icons-angular';
+import { iconSubset } from '../../../icons/icon-subset';
+import { playHallComponent } from './playhall.component';
+
+describe('RegisterComponent', () => {
+  let component: playHallComponent;
+  let fixture: ComponentFixture<playHallComponent>;
+  let iconSetService: IconSetService;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ playHallComponent ],
+      imports: [CardModule, FormModule, GridModule, ButtonModule, IconModule],
+      providers: [IconSetService]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    iconSetService = TestBed.inject(IconSetService);
+    iconSetService.icons = { ...iconSubset };
+
+    fixture = TestBed.createComponent(playHallComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ WEB/src/app/views/pages/playHall/playHall.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'play-Hall',
+  templateUrl: './playHall.component.html',
+  styleUrls: ['./playHall.component.scss']
+})
+export class playHallComponent {
+
+  constructor() { }
+
+}
+
